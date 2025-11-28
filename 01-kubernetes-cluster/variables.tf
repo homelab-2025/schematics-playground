f25c62c52bef9701652b893c6989211fb7cef0af
Enzo G <gl@enzog.mozmail.com>
2025-11-28 15:50:48 +0100

    Refactor variable definitions in variables.tf for improved readability

--- 01-kubernetes-cluster/variables.tf
+++ 01-kubernetes-cluster/variables.tf
@@ -16,31 +16,32 @@ variable "vpc_name" {
 variable "subnet_name" {
   default = "tf-subnet"
   type    = string
-
 }
 
 variable "subnet_azs" {
   default = "eu-de-1"
   type    = string
+}
 
+variable "subnet_total_ips" {
+  description = "Total number of IP addresses in the subnet"
+  default     = 256
+  type        = number
 }
 
 variable "k8s_cluster_name" {
   default = "tf-k8s-cluster"
   type    = string
-
 }
 
 variable "k8s_version" {
   default = "1.33.5"
   type    = string
-
 }
 
 variable "k8s_cluster_flavor" {
   default = "bx2.2x8" # minimum flavor for k8s cluster
   type    = string
-
 }
 
 variable "k8s_worker_count" {
