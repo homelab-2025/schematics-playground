01b3a2daad67e2603d288d5d6358f30109cd9e06
Enzo G <gl@enzog.mozmail.com>
2025-11-28 16:24:34 +0100

    Add output for public gateway ID and reorganize existing outputs

--- 02-openshift-cluster/outputs.tf
+++ 02-openshift-cluster/outputs.tf
@@ -6,10 +6,18 @@ output "vpc_id" {
   value = ibm_is_vpc.vpc.id
 }
 
+output "public_gateway_id" {
+  value = ibm_is_public_gateway.public_gateway.id
+}
+
 output "subnet_id" {
   value = ibm_is_subnet.subnet.id
 }
 
-output "k8s_cluster_id" {
-  value = ibm_container_vpc_cluster.k8s_cluster.id
+output "cos_id" {
+  value = ibm_resource_instance.cos_instance.id
+}
+
+output "openshift_cluster_id" {
+  value = ibm_container_vpc_cluster.openshift_cluster.id
 }
