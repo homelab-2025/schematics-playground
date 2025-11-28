7ea90cd21172e31b3fca7999d791e41a98a8dd58
Enzo G <gl@enzog.mozmail.com>
2025-11-28 13:38:37 +0100

    Add initial Terraform configuration for IBM Cloud resource group

---
+++ 00-resource-group/outputs.tf
@@ -0,0 +1,3 @@
+output "resource_group_id" {
+  value = ibm_resource_group.resource_group.id
+}
---
+++ 00-resource-group/variables.tf
@@ -0,0 +1,8 @@
+# Variables for IBM Cloud Provider
+variable "ibmcloud_api_key" {}
+variable "region" {}
+
+# Variables for Resource Group
+variable "resource_group_name" {
+  default = "terraform-rg"
+}
